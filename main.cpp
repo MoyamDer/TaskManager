54031bd4194fd6efc9340ce2542e2dfc5e7751da
MoyamDer <danilko9797@mail.ru>
2025-10-07 23:08:19 +0000

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+void addTask(std::string*& mass, int& size){
+    std::string selection;
+    std::string task; 
+    std::cout << "enter 'exit' to back to menu" << std::endl;
+
+    while(true){
+        std::cout << "add a task's: " << std::endl;
+        std::getline(std::cin, task);
+        if (task == "exit") break;
+        std::string* newMass = new std::string[size+1];
+        for (int i{0}; i != size; i++){
+            newMass[i] = mass[i];
+        }
+        newMass[size] = task;
+
+        delete[] mass;
+        mass = newMass;
+        size++;
+    }
+    
+}
+
+void tasksList(std::string*& mass, int& size){
+    std::string selection; 
+    for (unsigned int i{0}; i != size; i++){
+        std::cout << i+1 << ": " << mass[i] << "\n";
+    }
+    while(true){
+        std::cout << "enter 'exit' to back to menu" << std::endl;
+        std::cin >> selection;
+        if (selection == "exit"){
+            return;
+        }
+    }
+
+}
+
+void deleteTask(std::string*& mass, int& size){
+    std::string selection;
+    while (true){
+        for (unsigned int i{0}; i != size; i++){
+            std::cout << i + 1 << ": " << mass[i] << "\n";
+        }
+        std::cout << "enter tasks which you would like to delete" << std::endl;
+        std::cout << "enter 'exit' to back to menu" << std::endl;
+        unsigned int numberDel = {};
+        std::cin >> numberDel;
+        std::string* newMass = new std::string[size-1];
+        for (int i{0}, g{0}; i != size; i++){
+            if (i != numberDel - 1){
+            newMass[g++] = mass[i];
+            }
+        }
+        delete[]mass;
+        mass = newMass;
+        size--;
+    
+        std::cout << "wanna exit?" << std::endl;
+        std::cin >> selection;
+        if (selection == "exit"){
+            return;
+        }
+    }
+}
+
+int main(){
+    int size {0};
+    std::string selection {};
+    std::string* mass {nullptr};
+    do{
+        std::cout << "====== MENU ======" << std::endl;
+        std::cout << "1. Add task" << std::endl;
+        std::cout << "2. Delete task" << std::endl;
+        std::cout << "3. Task's list" << std::endl;
+        std::cout << "Enter section: " << std::endl;
+        std::cout << "Enter 'exit' to exit from programm" << std::endl;
+        std::cin >> selection;
+        if (selection == "1") addTask(mass, size);
+        if (selection == "2") deleteTask(mass, size);
+        if (selection == "3") tasksList(mass, size);
+    } while(selection != "exit");
+    delete[] mass;
+}

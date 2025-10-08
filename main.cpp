122fad7ac45bd5e738bd41b86dac0084e7655af0
MoyamDer <danilko9797@mail.ru>
2025-10-08 18:24:20 +0000

    add main updt.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <limits>
 
 void addTask(std::string*& mass, int& size){
     std::string selection;
     std::string task; 
+    std::cout << "======ADD MENU======" << "\n" << "\n";
     std::cout << "enter 'exit' to back to menu" << std::endl;
 
     while(true){
         std::cout << "add a task's: " << std::endl;
         std::getline(std::cin, task);
-        if (task == "exit") break;
+        if (task == "exit"){
+            system("cls");
+            break;
+        }
         std::string* newMass = new std::string[size+1];
         for (int i{0}; i != size; i++){
             newMass[i] = mass[i];
@@ -23,7 +28,8 @@ void addTask(std::string*& mass, int& size){
 }
 
 void tasksList(std::string*& mass, int& size){
-    std::string selection; 
+    std::string selection;
+    std::cout << "======TASK LIST======" << std::endl; 
     for (unsigned int i{0}; i != size; i++){
         std::cout << i+1 << ": " << mass[i] << "\n";
     }
@@ -31,6 +37,7 @@ void tasksList(std::string*& mass, int& size){
         std::cout << "enter 'exit' to back to menu" << std::endl;
         std::cin >> selection;
         if (selection == "exit"){
+            system("cls");
             return;
         }
     }
@@ -40,14 +47,22 @@ void tasksList(std::string*& mass, int& size){
 void deleteTask(std::string*& mass, int& size){
     std::string selection;
     while (true){
+        std::cout << "enter 'exit' to back to menu" << std::endl;
+        std::cout << "to delete task type anithing" << std::endl;
+        std::cin >> selection;
+        if (selection == "exit"){
+            system("cls");
+            return;
+        }
+        else{
+        system("cls");
         for (unsigned int i{0}; i != size; i++){
             std::cout << i + 1 << ": " << mass[i] << "\n";
         }
         std::cout << "enter tasks which you would like to delete" << std::endl;
-        std::cout << "enter 'exit' to back to menu" << std::endl;
         unsigned int numberDel = {};
         std::cin >> numberDel;
-        std::string* newMass = new std::string[size-1];
+         std::string* newMass = new std::string[size-1];
         for (int i{0}, g{0}; i != size; i++){
             if (i != numberDel - 1){
             newMass[g++] = mass[i];
@@ -56,11 +71,6 @@ void deleteTask(std::string*& mass, int& size){
         delete[]mass;
         mass = newMass;
         size--;
-    
-        std::cout << "wanna exit?" << std::endl;
-        std::cin >> selection;
-        if (selection == "exit"){
-            return;
         }
     }
 }
@@ -70,16 +80,26 @@ int main(){
     std::string selection {};
     std::string* mass {nullptr};
     do{
-        std::cout << "====== MENU ======" << std::endl;
+        std::cout << "====== MENU ======" << "\n" << "\n";
         std::cout << "1. Add task" << std::endl;
         std::cout << "2. Delete task" << std::endl;
         std::cout << "3. Task's list" << std::endl;
         std::cout << "Enter section: " << std::endl;
         std::cout << "Enter 'exit' to exit from programm" << std::endl;
         std::cin >> selection;
-        if (selection == "1") addTask(mass, size);
-        if (selection == "2") deleteTask(mass, size);
-        if (selection == "3") tasksList(mass, size);
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if (selection == "1"){
+            system("cls");
+            addTask(mass, size); 
+        }
+        if (selection == "2"){
+            system("cls");
+            deleteTask(mass, size);
+        }
+        if (selection == "3"){
+            system("cls");
+            tasksList(mass, size);
+        }
     } while(selection != "exit");
     delete[] mass;
 }
